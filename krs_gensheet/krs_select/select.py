d3bb8ef428469080f8bd7e4cc9c111cb8f7baada
pestophagous <pestophagous@users.noreply.github.com>
2025-02-08 12:00:56 -0800

    convert and aggregate input items into pages

--- krs_gensheet/krs_select/select.py
+++ krs_gensheet/krs_select/select.py
@@ -1,4 +1,6 @@
-from krs_pageassemble import page
+import random
+
+from krs_pageassemble import assemble, page
 
 
 class Selector:
@@ -8,18 +10,67 @@ class Selector:
         self._inputset = inputset
 
     def get_worksheets(self):
-        # TODO-NEXT: use the real inputset! we have it now.
-        print('in get_worksheets:')
-        print(self._inputset._ordered_items)
-        print('^^ in get_worksheets')
-        assert False
-        s = page.WorksheetPage(
-            unique_ids=['a844695'],
-            prompts=['$$ 4x^2 - 4 = 0 $$'])
-        return [s]
+        # FUTURE: Selector will filter by tag or other criteria.
+        sheets = []
+
+        # We shuffle for worksheets. We >sort< for answer key. do we need copies?
+        random.shuffle(self._inputset._ordered_items)
+
+        ids_on_one_page = []
+        prompts_on_one_page = []
+        for item in self._inputset._ordered_items:
+            ids_on_one_page.append(item.unique_id)
+            prompts_on_one_page.append(item.prompt)
+
+            if len(ids_on_one_page) == assemble._ITEMS_PER_WS_PAGE:
+                # make a WorksheetPage, add it to sheets list.
+                # reset 2 parallel lists.
+                p = page.WorksheetPage(
+                    unique_ids=ids_on_one_page,
+                    prompts=prompts_on_one_page)
+                sheets.append(p)
+                ids_on_one_page = []
+                prompts_on_one_page = []
+
+        if ids_on_one_page:
+            # "stragglers" that weren't a multiple of _ITEMS_PER_WS_PAGE
+            p = page.WorksheetPage(
+                unique_ids=ids_on_one_page,
+                prompts=prompts_on_one_page)
+            sheets.append(p)
+
+        return sheets
 
     def get_answerkeys(self):
-        s = page.AnswerKeyPage(
-            unique_ids=['a844695'],
-            answers=['x=1, x=-1'])
-        return [s]
+        # Even when WorksheetPage(s) are generated with some filtering, it is
+        # debatable if we ever need to filter the answers
+        keypages = []
+
+        # We shuffle for worksheets. We >sort< for answer key. do we need copies?
+        sorted_items = sorted(self._inputset._ordered_items,
+                              key=lambda x: x.unique_id)
+
+        ids_on_one_page = []
+        answers_on_one_page = []
+        for item in sorted_items:
+            ids_on_one_page.append(item.unique_id)
+            answers_on_one_page.append(item.answer)
+
+            if len(ids_on_one_page) == assemble._ITEMS_PER_AK_PAGE:
+                # make a AnswerKeyPage, add it to keypages list.
+                # reset 2 parallel lists.
+                p = page.AnswerKeyPage(
+                    unique_ids=ids_on_one_page,
+                    prompts=answers_on_one_page)
+                keypages.append(p)
+                ids_on_one_page = []
+                answers_on_one_page = []
+
+        if ids_on_one_page:
+            # "stragglers" that weren't a multiple of _ITEMS_PER_AK_PAGE
+            p = page.AnswerKeyPage(
+                unique_ids=ids_on_one_page,
+                answers=answers_on_one_page)
+            keypages.append(p)
+
+        return keypages
